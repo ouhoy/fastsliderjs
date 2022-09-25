e1c85728f1c7314165dbaf6a34e27ae1a00b82cf
ouhoy <kimkinon@gmail.com>
2022-09-25 18:45:56 +0100

    Make the HTML/CSS layout -fullwidth fodler-

--- test/full-width/index.js
+++ test/full-width/index.js
@@ -1,1 +1,14 @@
 import { Slider } from "../../src/fastSlider.js";
+import { getAverageColor } from "../../src/getAverageColor.js";
+
+function $(id, slectAll = false) {
+  return slectAll ? document.querySelectorAll(id) : document.querySelector(id);
+}
+
+const image = $("img");
+
+if (image.complete) {
+  const { R, G, B } = getAverageColor(image, 4);
+  document.body.style.background = `rgb(${R}, ${G},${B})`;
+}
+const slider1 = new Slider(".slider-container");
